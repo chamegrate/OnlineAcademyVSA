180698eea22c34711150219691a9a430a9dd1581
chamegrate <vsa.mathieu@gmail.com>
2020-03-02 11:02:12 +0100

    eertseOpdracht

--- mainscript.js
+++ mainscript.js
@@ -1,1 +1,12 @@
-console.log("Het bestand mainscript.js is met succes geladen")
+var bedrijfNaam = "Online Academie Mathieu";
+var straatNaam = "Grote Markt";
+var straatNummer = "1";
+var postCode ="3000";
+var gemeente = "Leuven";
+var btwNummer = "BE0999999999";
+var btwPlichtig = true;
+var webSiteAdres = "www.itenzo.be";
+var mailAdres = "vsa.mathieu@gmail.com";
+var bedrijAdres = bedrijfNaam + " " + straatNaam  + " " + straatNummer + " " + postCode + " " + gemeente;
+
+document.getElementById("bedrijfsgegevens").innerHTML = bedrijAdres
